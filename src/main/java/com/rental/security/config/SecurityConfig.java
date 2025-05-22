513c2d4e1701f2e6ca23a34cce72396caf302c44
Prateek Kumar <Prateek.kumar949@gmail.com>
2025-05-22 21:03:11 +0530

    Done changes

---
+++ src/main/java/com/rental/security/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.rental.security.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/rental/security/model/CustomUser.java
@@ -0,0 +1,5 @@
+package com.rental.security.model;
+
+public class CustomUser {
+
+}
