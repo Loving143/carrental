590c715a5bbb831a13f21a05c2d0df7ff477c815
Prateek Kumar <prateek.kumar949@gmail.com>
2025-05-22 14:04:19 +0530

    Done some work

---
+++ src/main/java/com/carrental/enumm/Gender.java
@@ -0,0 +1,5 @@
+package com.carrental.enumm;
+
+public enum Gender {
+	MALE,FEMALE
+}
---
+++ src/main/java/com/carrental/util/Address.java
@@ -0,0 +1,8 @@
+package com.carrental.util;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Address {
+
+}
